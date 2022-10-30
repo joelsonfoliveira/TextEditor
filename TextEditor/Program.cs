5ff1e8c17d67865be96dc77e594bd723a2fca423
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-29 23:32:20 -0300

    Iniciando o projeto

---
+++ TextEditor/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace TextEditor
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      System.Console.WriteLine("O que você deseja fazer?");
+      System.Console.WriteLine("1 - Abrir Arquivo");
+      System.Console.WriteLine("2 - Criar novo arquivo");
+      System.Console.WriteLine("0 - Sair");
+      short opcao = short.Parse(Console.ReadLine());
+
+      switch (opcao)
+      {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Editar(); break;
+        default: Menu(); break;
+      }
+    }
+
+    static void Abrir() { }
+
+    static void Editar() { }
+  }
+}
