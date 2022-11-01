f4aadaf70728511dc447377cc3e7aee6e6ca07be
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-11-01 09:44:44 -0300

    Abrindo arquivos

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -29,7 +29,24 @@ namespace TextEditor
       }
     }
 
-    static void Abrir() { }
+    static void Abrir()
+    {
+      string path = "";
+
+      Console.Clear();
+      Console.WriteLine("Qual o caminho do arquivo?");
+      path = Console.ReadLine();
+
+      using (var file = new StreamReader(path!))
+      {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+      }
+
+      System.Console.WriteLine("");
+      Console.ReadLine();
+      Menu();
+    }
 
     static void Editar()
     {
